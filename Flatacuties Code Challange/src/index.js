cdf0e7dd3a21bb533f753035d5cf6e472c631fe8
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-12-06 08:44:43 +0300

    Update index.js

--- Flatacuties Code Challange/src/index.js
+++ Flatacuties Code Challange/src/index.js
@@ -1,39 +1,54 @@
 // Your code here
-const character = document.getElementById("character-bar");
-const container = document.getElementById("detailed-info")
-const animalName = document.getElementById("name");
-const image = document.getElementById("image");
-const form = document.getElementById("votes-form")
-const animalVotes = document.getElementById("vote-count");
-const input = document.getElementById("votes");
-const resetVotes = document.getElementById('reset-btn');resetVotes.style.cursor = 'pointer';let currentAnimal;form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    currentAnimal.votes += parseInt(e.target.votes.value);
-    showAnimal(currentAnimal);
-});
-resetVotes.addEventListener('click', () => {
-    currentAnimal.votes = 0;
-    showAnimal(currentAnimal);
-})
-console.log("steve")
-function getCharacters() {
-    fetch('http://localhost:3000/characters/')
+const character = document.getElementById('character-bar');
+// const container = document.getElementById("detailed-info");
+const animalName = document.getElementById('name');
+const image = document.getElementById('image');
+const form = document.getElementById('votes-form');
+const animalVotes = document.getElementById('vote-count');
+const input = document.getElementById('votes');
+const resetVotes = document.getElementById('reset-btn');
+let currentAnimal;
+resetVotes.style.cursor = 'pointer';
+
+// endpoint for fetch data
+function getCharacters () {
+  fetch('http://localhost:3000/characters/')
     .then(response => response.json())
-    .then(renderAnimals)
+    .then(renderAnimals);
 }
-getCharacters();function renderAnimals(animals) {
-    animals.forEach(renderCharacters)
-}function renderCharacters(animal) {
-    const spanEle = document.createElement('span')
-    spanEle.innerHTML = animal.name;
-    spanEle.style.cursor = 'pointer';
-    character.appendChild(spanEle)
-    spanEle.addEventListener('click', () => {
-        currentAnimal = animal;
-        showAnimal(animal);
-})
-}function showAnimal(animal){
-    animalName.innerHTML = animal.name;
-    image.src = animal.image;
-    animalVotes.innerHTML = animal.votes;
+// function that accesses the payload
+function renderAnimals (animals) {
+  animals.forEach(renderCharacters);
 }
+
+// function that accesses individual payload elements and show their required data
+function renderCharacters (animal) {
+  const spanEle = document.createElement('span');
+  spanEle.innerHTML = animal.name;
+  spanEle.style.cursor = 'pointer';
+  character.appendChild(spanEle);
+  spanEle.addEventListener('click', () => {
+    currentAnimal = animal;
+    showAnimal(animal);
+  });
+}
+
+// A function that dsplays curated data about the animal in question
+function showAnimal (animal) {
+  animalName.innerHTML = animal.name;
+  image.src = animal.image;
+  animalVotes.innerHTML = animal.votes;
+}
+getCharacters();
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  currentAnimal.votes += parseInt(e.target.votes.value);
+  showAnimal(currentAnimal);
+  form.reset();
+});
+
+resetVotes.addEventListener('click', () => {
+  currentAnimal.votes = 0;
+  showAnimal(currentAnimal);
+});
